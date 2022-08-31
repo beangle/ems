a223d33d8d09ce029cfcb1f7ff3277a85c40be6b
段体华 <duantihua@gmail.com>
2022-08-31 21:56:51 +0800

    fix migrate sql spell bug

--- core/src/main/resources/db/postgresql/migrate/4.4.7.sql
+++ core/src/main/resources/db/postgresql/migrate/4.4.7.sql
@@ -1,4 +1,4 @@
 
 alter table bulletin.notices add issuer varchar(40);
 update bulletin.notices n set issuer = (select u.name from usr.users u where u.id=n.operator_id);
-alter table bulletin.notice alter column issuer set not null;
+alter table bulletin.notices alter column issuer set not null;
