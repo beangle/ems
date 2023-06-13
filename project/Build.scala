3da85296c491020c288e381c5be118906612fbdb
段体华 <duantihua@gmail.com>
2023-06-13 13:27:59 +0800

    bump version 4.6.18

--- project/Build.scala
+++ project/Build.scala
@@ -10,8 +10,8 @@ object EmsDepends {
   val cacheVer = "0.1.4"
   val templateVer = "0.1.6"
   val webmvcVer = "0.9.3"
-  val securityVer = "4.3.8-SNAPSHOT"
-  val idsVer = "0.3.10-SNAPSHOT"
+  val securityVer = "4.3.8"
+  val idsVer = "0.3.10"
 
   val commonsCore = "org.beangle.commons" %% "beangle-commons-core" % commonsVer
   val commonsFile = "org.beangle.commons" %% "beangle-commons-file" % commonsVer
