0296fad2c75cf722dcf9d28dfe7fe38b87ff8814
段体华 <duantihua@gmail.com>
2023-11-17 00:47:02 +0800

    add local template detection

--- web/src/main/scala/org/beangle/ems/web/WebReconfigModule.scala
+++ web/src/main/scala/org/beangle/ems/web/WebReconfigModule.scala
@@ -20,10 +20,15 @@ package org.beangle.ems.web
 import org.beangle.cdi.bind.ReconfigModule
 import org.beangle.ems.app.{Ems, EmsApp}
 
+import java.io.File
+
 class WebReconfigModule extends ReconfigModule {
   override protected def config(): Unit = {
-    //support load local freemarker template files
-    update("mvc.FreemarkerConfigurer.default")
-      .set("templatePath", s"file://${Ems.home + EmsApp.path}/pages/,class://")
+    val file = new File(Ems.home + EmsApp.path + "/pages")
+    if (file.exists()) {
+      //support load local freemarker template files
+      update("mvc.FreemarkerConfigurer.default")
+        .set("templatePath", s"file://${Ems.home + EmsApp.path}/pages/,class://")
+    }
   }
 }
