bdb0f68565fad1dae2201daad8e2a0b8d3501951
段体华 <duantihua@gmail.com>
2025-06-13 12:09:45 +0800

    add migrate sql

---
+++ core/src/main/resources/db/postgresql/migrate/4.11.3.sql
@@ -0,0 +1,1 @@
+alter table ems.usr_users add openid varchar(60);
--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import org.beangle.parent.Dependencies.*
 import sbt.*
 
 object EmsDepends {
-  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.28"
+  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.29"
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.0.11"
   val beangle_model = "org.beangle.data" % "beangle-model" % "5.8.24"
   val beangle_cdi = "org.beangle.cdi" % "beangle-cdi" % "0.7.3"
@@ -13,7 +13,7 @@ object EmsDepends {
   val beangle_bui_tag = "org.beangle.bui" % "beangle-bui-tag" % "0.0.6"
   val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.0.6"
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.18"
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.3.29-SNAPSHOT"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.3.29"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.3.24"
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.0"
   val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.13"
