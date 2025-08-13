af373a12340f5e84510fc3eb7bd50a26262f2f5d
段体华 <duantihua@gmail.com>
2025-08-13 20:16:41 +0800

    update to data 5.9.1

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt.*
 object EmsDepends {
   val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.30"
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.0.12"
-  val beangle_model = "org.beangle.data" % "beangle-model" % "5.9.0"
+  val beangle_model = "org.beangle.data" % "beangle-model" % "5.9.1"
   val beangle_cdi = "org.beangle.cdi" % "beangle-cdi" % "0.7.4"
   val beangle_web = "org.beangle.web" % "beangle-web" % "0.6.5"
   val beangle_cache = "org.beangle.cache" % "beangle-cache" % "0.1.15"
@@ -16,7 +16,7 @@ object EmsDepends {
   val beangle_security = "org.beangle.security" % "beangle-security" % "4.3.30"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.3.25"
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.1"
-  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.15"
+  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.16"
 
   val appDepends = Seq(beangle_commons, logback_classic, scalatest, beangle_web) ++
     Seq(beangle_cdi, beangle_jdbc, spring_beans, spring_context, spring_tx, spring_jdbc, HikariCP) ++
