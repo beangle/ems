b02e429e431d553d481195894a48007de7759dfc
段体华 <duantihua@gmail.com>
2025-10-27 14:50:46 +0800

    update to parent 0.15.0

--- project/Build.scala
+++ project/Build.scala
@@ -2,21 +2,21 @@ import org.beangle.parent.Dependencies.*
 import sbt.*
 
 object EmsDepends {
-  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.31"
-  val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.0"
-  val beangle_model = "org.beangle.data" % "beangle-model" % "5.9.2"
-  val beangle_cdi = "org.beangle.cdi" % "beangle-cdi" % "0.7.4"
-  val beangle_web = "org.beangle.web" % "beangle-web" % "0.6.5"
-  val beangle_cache = "org.beangle.cache" % "beangle-cache" % "0.1.15"
-  val beangle_template = "org.beangle.template" % "beangle-template" % "0.1.27"
-  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.10.7"
-  val beangle_bui_tag = "org.beangle.bui" % "beangle-bui-tag" % "0.0.8"
-  val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.0.8"
-  val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.19"
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.0"
-  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.0"
-  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.1"
-  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.16"
+  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.32"
+  val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.3"
+  val beangle_model = "org.beangle.data" % "beangle-model" % "5.10.0"
+  val beangle_cdi = "org.beangle.cdi" % "beangle-cdi" % "0.7.5"
+  val beangle_web = "org.beangle.web" % "beangle-web" % "0.6.6"
+  val beangle_cache = "org.beangle.cache" % "beangle-cache" % "0.1.16"
+  val beangle_template = "org.beangle.template" % "beangle-template" % "0.1.28"
+  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.11.0"
+  val beangle_bui_tag = "org.beangle.bui" % "beangle-bui-tag" % "0.0.9"
+  val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.0.9"
+  val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.20"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.1"
+  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.1"
+  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.2"
+  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.18"
 
   val appDepends = Seq(beangle_commons, logback_classic, scalatest, beangle_web) ++
     Seq(beangle_cdi, beangle_jdbc, spring_beans, spring_context, spring_tx, spring_jdbc, HikariCP) ++
