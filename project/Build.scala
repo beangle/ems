c26db998d0b4efe3ebba0a30d7216161394e6993
段体华 <duantihua@gmail.com>
2026-02-16 04:42:39 +0800

    update tools 0.0.26

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object EmsDepends {
   val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.5"
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.4"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.8"
-  val beangle_model = "org.beangle.data" % "beangle-model" % "5.12.0"
+  val beangle_data_hibernate = "org.beangle.data" % "beangle-data-hibernate" % "5.12.0"
   val beangle_notify = "org.beangle.notify" % "beangle-notify" % "0.1.20"
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.8"
   val beangle_she = "org.beangle.she" % "beangle-she" % "0.0.1"
@@ -21,11 +21,11 @@ object EmsDepends {
 
   val appDepends = Seq(beangle_config, typesafe_config, slf4j, logback_classic) ++
     Seq(beangle_cdi, spring_beans) ++
-    Seq(beangle_jdbc, spring_tx, spring_aop, HikariCP, postgresql) ++
+    Seq(beangle_jdbc, HikariCP, postgresql) ++
     Seq(beangle_cache, caffeine_jcache, jedis) ++
-    Seq(beangle_model, hibernate_core, hibernate_jcache) ++
+    Seq(beangle_data_hibernate, spring_tx, spring_aop) ++
     Seq(beangle_security, beangle_template, beangle_serializer, beangle_event) ++
     Seq(scalatest)
 
-  val webAppDepends = appDepends ++ Seq(beangle_she, freemarker)
+  val webAppDepends = appDepends ++ Seq(beangle_she)
 }
--- project/plugin.sbt
+++ project/plugin.sbt
@@ -1,3 +1,3 @@
 addSbtPlugin("org.beangle.parent" % "sbt-beangle-parent" % "0.15.11")
 addSbtPlugin("org.beangle.build" % "sbt-beangle-build" % "0.0.20")
-addSbtPlugin("org.beangle.tools" % "sbt-beangle-tools" % "0.0.25")
+addSbtPlugin("org.beangle.tools" % "sbt-beangle-tools" % "0.0.26")
