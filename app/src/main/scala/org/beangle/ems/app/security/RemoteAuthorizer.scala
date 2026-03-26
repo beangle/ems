7e081dc23a17bf5f5b7fd285fa05f0cb8bdd8112
段体华 <duantihua@gmail.com>
2026-03-27 07:46:59 +0800

    fix RemoteAuthorizer refresh with async

--- app/src/main/scala/org/beangle/ems/app/security/RemoteAuthorizer.scala
+++ app/src/main/scala/org/beangle/ems/app/security/RemoteAuthorizer.scala
@@ -19,14 +19,26 @@ package org.beangle.ems.app.security
 
 import org.beangle.security.authz.{AbstractRoleBasedAuthorizer, AuthorityDomain}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 /**
-  * @author chaostone
-  */
+ * @author chaostone
+ */
 class RemoteAuthorizer extends AbstractRoleBasedAuthorizer {
 
   override def fetchDomain(): AuthorityDomain = {
     val roots = RemoteService.roots
     val resources = RemoteService.getAuthorities
     AuthorityDomain(roots.getOrElse(Set.empty), resources)
+  }
+
+  /**
+   * 此处一定要重载，因为获取权限需要访问网络服务，有可能该服务就是自身进程提供的，容易卡住主线程
+   */
+  override def init(): Unit = {
+    Future {
+      refresh()
+    }
   }
 }
