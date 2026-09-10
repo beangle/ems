16e68ed2044cf3c8899a8ff3a55bf117f60471a7
chaostone <duantihua@gmail.com>
2026-09-10 19:28:22 +0800

    update to security 4.5.2

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object EmsDepends {
   val beangle_notify = "org.beangle.notify" % "beangle-notify" % "0.1.27"
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.16"
   val beangle_she = "org.beangle.she" % "beangle-she" % "0.0.18"
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.5.2-SNAPSHOT"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.5.2"
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.28"
   val beangle_template = "org.beangle.template" % "beangle-template" % "0.2.12"
   val beangle_transfer = "org.beangle.transfer" % "beangle-transfer" % "0.0.8"
